949995b33fb42eb1fe1850963ef4136f413eabb0
Joonsung <jejess3@gmail.com>
2025-02-26 13:36:44 -0500

    Update ui.ts
    edited some UI tools

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const CHAT_HEADER: string = `Hi There!`;
+export const MESSAGE_PLACEHOLDER: string = `MSG placeholder`;
 export const FOOTER_MESSAGE: string = `Customized by JS, UNC KFBS`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const CLEAR_BUTTON_TEXT: string = `I'm an Eraser!`;
+export const PAGE_TITLE: string = `Title of Page`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
