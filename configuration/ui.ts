4db440f81df377f74bf4737897b223b842f292c3
Joonsung <jejess3@gmail.com>
2025-02-26 14:17:23 -0500

    Update ui.ts
    page title change

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `Hi There!`;
 export const MESSAGE_PLACEHOLDER: string = `MSG placeholder`;
 export const FOOTER_MESSAGE: string = `Customized by JS, UNC KFBS`;
 export const CLEAR_BUTTON_TEXT: string = `I'm an Eraser!`;
-export const PAGE_TITLE: string = `Title of Page`;
+export const PAGE_TITLE: string = `I'm EL-JS's Chatbot!`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
